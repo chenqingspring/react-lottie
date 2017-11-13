b5a55e47ba20a203f94bbf38780487f950c2b3d3
jwfwessels <jwfwessels@gmail.com>
2017-11-13 06:48:16 +0200

    Add eventhandler example to storybook
    Added transition loop example, which illustrates how an event can be
    used to trigger state changes.
    Made note of broken loop prop, to investigate.
    added babel-eslint parser setting to eslint (babel-eslint
    was already a project dependancy).

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {storiesOf, action} from '@kadira/storybook';
-import LottieControl from './lottie-control'
-import ToggleLike from "./toggle-like";
+import { storiesOf, action } from '@kadira/storybook';
+import LottieControl from './lottie-control';
+import ToggleLike from './toggle-like';
+import TransitionLoop from './TransitionLoop';
 
 storiesOf('Lottie Animation View', module)
-  .add('with control', () => (
-    <LottieControl/>
-  )).add('toggle like', () => (
-  <ToggleLike/>
-));
+  .add('with control', () => <LottieControl />)
+  .add('toggle like', () => <ToggleLike />)
+  .add('transitions & loops', () => <TransitionLoop />);
