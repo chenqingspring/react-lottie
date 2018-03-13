70bd08cceb0f86fa69737e8173e0085ddc0afb9d
Elliot Cobb <elliot@turo.com>
2018-03-12 19:05:29 -0700

    story(TransitionWithOptions) add story demonstrating how new options can be passed with a new animation

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,8 +3,10 @@ import { storiesOf, action } from '@kadira/storybook';
 import LottieControl from './lottie-control';
 import ToggleLike from './toggle-like';
 import TransitionLoop from './TransitionLoop';
+import TransitionWithOptions from './TransitionWithOptions';
 
 storiesOf('Lottie Animation View', module)
   .add('with control', () => <LottieControl />)
   .add('toggle like', () => <ToggleLike />)
-  .add('transitions & loops', () => <TransitionLoop />);
+  .add('transitions & loops', () => <TransitionLoop />)
+  .add('transitions with options', () => <TransitionWithOptions />);
