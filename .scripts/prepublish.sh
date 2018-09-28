679f904921c71537d39d4b36c5fe5480669074a3
Frederik Hummel <frederik.hummel@bumbledev.com>
2018-09-28 13:51:36 +0200

    Added type definitions for TypeScript compatibility

--- .scripts/prepublish.sh
+++ .scripts/prepublish.sh
@@ -12,5 +12,11 @@ rm -rf ./dist
 NODE_ENV=production ./node_modules/.bin/babel --ignore tests,stories --plugins "transform-runtime" ./src --out-dir ./dist
 echo ""
 echo "=> Transpiling completed."
+echo ""
+echo "=> Copying type definitions ..."
+echo ""
+cp ./src/index.d.ts ./dist/index.d.ts
+echo ""
+echo "=> Copying completed."
 
 . .scripts/user/prepublish.sh
