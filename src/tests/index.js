f8ef4078fb1cecc55812927a13f145113879579d
Ashok Suthar <coderatlabs@gmail.com>
2022-03-24 10:32:11 +0100

    silence lint rules pulled in after airbnb eslint update

--- src/tests/index.js
+++ src/tests/index.js
@@ -45,7 +45,7 @@ describe('react-lottie', () => {
             ariaRole="test"
             ariaLabel="testlabel"
             title="title"
-          />
+          />,
         );
 
         expect(component.find('div').prop('role')).to.equal('test');
@@ -61,7 +61,7 @@ describe('react-lottie', () => {
             options={defaultOptions}
             height={199}
             width={188}
-          />
+          />,
         );
 
         expect(component.find('div').prop('style').height).to.equal('199px');
