81251556917a54d0f351cf42168e9202458790e0
Ashok Suthar <coderatlabs@gmail.com>
2022-03-29 10:56:11 +0200

    upgrade storybook to latest version

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@
 // This is an auto generated file with React CDK.
 // Do not modify this file.
 
-import { configure } from '@kadira/storybook';
+import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/stories');
